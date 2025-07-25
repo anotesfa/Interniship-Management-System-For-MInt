0ab31687e13c85c9b69336b11e870c9e891ab396
anotesfa <anamtesfa@gmail.com>
2025-07-25 11:53:48 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello guys I'm come");
-  <h1>Heyyyyyy</h1>
+    res.end("Hello guys, I've come");
+  
 });
 
 server.listen(8000, '127.0.0.1', () => {
