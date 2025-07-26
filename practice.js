9fda59e84ae87cfbccde392067d36780ea51aba0
anotesfa <anamtesfa@gmail.com>
2025-07-26 11:33:26 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -2,9 +2,17 @@ const http=require('http');
 const url=require('url');
 
 const server= http.createServer((req,res)=>{
-  console.log(req.url)
-  res.end("Heyyyyyyyys");
+  const PathUrl=req.url;
+  if(PathUrl==='/' || PathUrl==='/overview'){
+    res.end("This is OVERVIEW!");
+  }
+  else if(PathUrl==='/product'){
+    res.end("This is PRODUCT!");
+  }
+  else {
+    res.end("Page not found");
+  }
 })
 server.listen(8000,'127.0.0.1',()=>{
-  console.log("This is checking for doing of it!");
+  console.log("This is checking for doing it!");
 })
