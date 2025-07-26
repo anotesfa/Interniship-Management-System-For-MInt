9839933adc2a93a3fbe5aa4db4495e9fe2b4a43e
anotesfa <anamtesfa@gmail.com>
2025-07-26 11:24:19 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,11 +1,10 @@
-const http = require('http');
+const http=require('http');
+const url=require('url');
 
-const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end("Hello guys, I've come");
-  
-});
-
-server.listen(3000, '127.0.0.1', () => {
-  console.log("This is from server");
-});
+const server= http.createServer((req,res)=>{
+  console.log(req.url)
+  res.end("Heyyyyyyyys");
+})
+server.listen(8000,'127.0.0.1',()=>{
+  console.log("This is checking for doing of it!");
+})
