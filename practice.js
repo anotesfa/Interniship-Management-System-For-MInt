3964a378b02c7aa061b840aaa26404f9d81fe2c8
anotesfa <anamtesfa@gmail.com>
2026-05-01 12:05:04 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -33,7 +33,7 @@ const server= http.createServer((req,res)=>{
     });
     res.end("<h1>Page not found</h1>");
   }
-}) 
+})  
 server.listen(8000,'127.0.0.1',()=>{
   console.log("This is checking for doing it!");
 })
